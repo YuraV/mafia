bf35a018f65d9fc167862bf66651a165bdc079ec
Yurii Vodolyak <yurii.vodolyak@onapp.com>
2013-09-23 16:28:57 +0300

    score

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,10 +1,9 @@
 class ScoresController < ApplicationController
   def index
-    @users_count = Appointment.where(user_id: '1').count()
+
   end
 
   def show
-    #@users = User.scoped
     @user = User.find(params[:id])
     @score = Appointment.where(user_id: @user.id).count()
   end
