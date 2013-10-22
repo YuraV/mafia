42fe50639b3b050941a4963b58ed399a362ce4a8
Yura Kuzhiy <yurakuzhiy@gmail.com>
2013-10-22 16:23:40 +0300

    all changes

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,8 +3,6 @@ class Appointment < ActiveRecord::Base
   belongs_to :user
 
   attr_accessible :user_id, :game_id, :id, :role, :team, :player_number, :remark
-
-
   validates :user_id, presence: true
 
 
@@ -23,8 +21,8 @@ class Appointment < ActiveRecord::Base
   end
 
   def set_score!
+   # roles = {'citizen'=>3,'don'=>5,'mafia'=>4,'loser':0,'main'=>-1,'sherif'=>4}
     if game.result == 'red'
-      self.update_attribute(:won, true)
       if self.team == 'red' && self.role == 'sherif'
         self.update_attribute(:score, 4 )
       elsif self.team == 'red'
@@ -33,7 +31,7 @@ class Appointment < ActiveRecord::Base
         self.update_attribute(:score, -1)
       elsif self.team =='black'
         self.update_attribute(:score, 0 )
-      end
+      end  
     elsif game.result == 'black'
       if self.team == 'black' && self.role == 'Don'
         self.update_attribute(:score, 5)
@@ -63,8 +61,6 @@ class Appointment < ActiveRecord::Base
         self.update_attribute(:won, true)
       end
     end
-
-
   end
 
 end
