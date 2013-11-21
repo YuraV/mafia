66de6a800bc1b58442aa0eb737f9f1abc538eee0
Yura Kuzhiy <morf17@gmail.com>
2013-11-22 00:35:45 +0200

    added popover for users roles image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require jquery.facebox
 //= require jquery.noSelectText
+//= require bootstrap 
 //= require_tree .
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -6,4 +6,13 @@ module GamesHelper
       actions[:destroy] = link_to 'Destroy', game_path(g), method: :delete,class:'btn btn-danger'
     render "shared/actions", :object => g, :actions => actions
   end
+
+  def user_alias_by_player player
+    user = User.find(player.user_id)
+  if player.player_number
+    "##{player.player_number}. #{user.alias} "
+    else
+    "#{user.alias}"
+    end 
+  end
 end
